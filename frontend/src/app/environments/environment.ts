8df3e4ddc94159fff0056b6987e44b4c5ec2e477
Robert Hoffmann <rob.hoffmann@posteo.de>
2023-11-19 18:11:50 +0100

    camera 10

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-//  baseUrl: 'http://localhost:8000/' //Base Url for Backend
-baseUrl: 'http://192.168.0.53:8000/' //Base Url for Backend
+  baseUrl: 'http://localhost:8000/' //Base Url for Backend
+//baseUrl: 'http://192.168.0.53:8000/' //Base Url for Backend
 };
